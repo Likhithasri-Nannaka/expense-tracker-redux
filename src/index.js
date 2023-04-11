b9281f48b94a69f3ea7e9e36d0d8571fc3734d4e
Likhithasri-Nannaka <likhithasri919@gmail.com>
2023-04-11 08:50:47 +0530

    successfully added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import store from "./Components/Store/Store";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
-import store from './store/index';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
